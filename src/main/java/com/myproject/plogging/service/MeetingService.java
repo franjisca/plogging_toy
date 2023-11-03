0f3bf259464563b66d9f72a6c4355c4320b2a6d4
JIYEON <publicjy324@gmail.com>
2023-11-03 20:38:33 +0900

    메일서비스 실행 로직 추가

--- src/main/java/com/myproject/plogging/service/MeetingService.java
+++ src/main/java/com/myproject/plogging/service/MeetingService.java
@@ -118,15 +118,15 @@ public class MeetingService {
         User user = userRepository.findByUserStrId(userId);
 
         boolean continueProcess = meetingRepository.alreadyEnjoyed(user.getId(), meetingNo);
+
         if(!continueProcess){
             Meeting meeting = meetingRepository.findById(meetingNo).orElseThrow(IllegalArgumentException::new);
             //더티체킹
             meeting.enjoyMeeting(user.getId());
+            mailService.whenEnjoyMeeting(user, meeting);
         }
 
-        Meeting meeting = meetingRepository.findById(meetingNo).orElseThrow(IllegalArgumentException::new);
 
-        mailService.whenEnjoyMeeting(user, meeting);
 
         throw new IllegalArgumentException("이미 참여중인 모임입니다.");
     }
