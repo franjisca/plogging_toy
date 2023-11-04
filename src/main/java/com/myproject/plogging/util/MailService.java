8bc1784ea26a1849f88d631851d04d8ba9506b77
JIYEON <publicjy324@gmail.com>
2023-11-04 19:56:03 +0900

    MailService 로직 수정

--- src/main/java/com/myproject/plogging/util/MailService.java
+++ src/main/java/com/myproject/plogging/util/MailService.java
@@ -18,10 +18,22 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void whenEnjoyMeeting(User user, Meeting meeting){
+    public void whenEnjoyMeeting(User user, Meeting meeting, boolean flag){
+
+        // 메시지 객체 생성
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject(user.getUsername()+"님 플로깅 모임에 참여하셨습니다!");
+
+        // 이메일 받을 대상
         message.setTo(user.getEmail());
+
+        if(flag) {
+                // 이메일 제목
+                message.setSubject(user.getUsername()+"님 플로깅 모임이 생성 되었습니다!");
+                message.setText(user.getUsername()+"님 플로깅 모임을 생성해주셔서 감사합니다. 즐거운 모임과 더 나은 지구를 위한 실천을 응원합니다!");
+        }
+
+        // flag = false
+        message.setSubject(user.getUsername()+"님 플로깅 모임에 참여하셨습니다!");
         message.setText(user.getUsername()+ "님 " +meeting.getTitle()+" 모임에 참여하셨습니다. " +
                 "즐거운 모임과 더 나은 지구를 위한 실천을 응원합니다!");
 
