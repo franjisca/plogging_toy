850931d88a50f01757e5a9585c0404d13a60b284
JIYEON <publicjy324@gmail.com>
2023-11-11 13:32:57 +0900

    사용하지 않는 import 삭제

--- src/main/java/com/myproject/plogging/util/MailService.java
+++ src/main/java/com/myproject/plogging/util/MailService.java
@@ -3,15 +3,10 @@ package com.myproject.plogging.util;
 
 import com.myproject.plogging.domain.Meeting;
 import com.myproject.plogging.domain.User;
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.spring6.SpringTemplateEngine;
