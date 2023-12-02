321a5ecba4b96004a8088b8459640077999777a5
JIYEON <publicjy324@gmail.com>
2023-12-02 13:21:39 +0900

    like count batch process 적용중

--- src/main/java/com/myproject/plogging/batch/likesjob/LikesDataJob.java
+++ src/main/java/com/myproject/plogging/batch/likesjob/LikesDataJob.java
@@ -1,6 +1,7 @@
 package com.myproject.plogging.batch.likesjob;
 
 
+import com.myproject.plogging.domain.PhotoList;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -14,6 +15,13 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -27,7 +35,7 @@ import org.springframework.transaction.TransactionStatus;
 @RequiredArgsConstructor
 public class LikesDataJob {
 
-    EntityManagerFactory em;
+    private final EntityManagerFactory em;
 
     @Bean
     public Job likedDataJob(JobRepository jobRepository){
@@ -57,6 +65,41 @@ public class LikesDataJob {
     public Step likesStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("likesStep", jobRepository)
                 .tasklet(likesTasklet(), transactionManager)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step dbDataStep(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager
+
+    ) {
+        return new StepBuilder("dbDataStep", jobRepository)
+                .<PhotoList, PhotoList>chunk(100, transactionManager)// 100개 단위 commit,
+                .reader(dbDataReader())
+                .processor(dbDataProcessor())
+                .writer(dbDataWriter())
+                .build();
+    }
+
+    private ItemWriter<? super PhotoList> dbDataWriter() {
+
+        return null;
+    }
+
+    private ItemProcessor<? super PhotoList,? extends PhotoList> dbDataProcessor() {
+
+        return null;
+    }
+
+    private ItemReader<PhotoList> dbDataReader() {
+        return new JpaCursorItemReaderBuilder<PhotoList>()
+                .name("dbReader")
+                .entityManagerFactory(em)
+                .queryString(
+                        "select o from PhotoList o"
+                )
                 .build();
     }
 
