6133ecb379c9c0a5554bbe23a25a4a9cfcfe7f03
JIYEON <publicjy324@gmail.com>
2024-01-02 21:21:37 +0900

    redis 설정 추가

---
+++ src/main/java/com/myproject/plogging/config/producer/PlasticbagProducer.java
@@ -0,0 +1,19 @@
+package com.myproject.plogging.config.producer;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlasticbagProducer {
+
+    private final KafkaTemplate<String, Long> kafkaTemplate;
+
+    public void create(Long userId) {
+        kafkaTemplate.send("plastic_create", userId);
+    }
+
+
+}
