e87a4f01eb668774bc0e61dd886a579117b3d0a0
JIYEON <publicjy324@gmail.com>
2024-01-11 20:47:11 +0900

    health check api

---
+++ src/main/java/com/myproject/plogging/controller/health/dto/HealthCheckResponseDto.java
@@ -0,0 +1,15 @@
+package com.myproject.plogging.controller.health.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class HealthCheckResponseDto {
+
+    private String health;
+    private List<String> activeProfiles;
+}
