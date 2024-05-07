88459a262fe2f07aaa15e1ccdd8d77f4b9a1f4b0
JIYEON <publicjy324@gmail.com>
2024-05-07 13:51:40 +0900

    클래스 삭제

--- src/main/java/com/myproject/plogging/repository/user/plasticbag/RedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.plogging.repository.user.plasticbag;
-
-import com.myproject.plogging.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RedisRepository extends JpaRepository<User, Long> {
-}
