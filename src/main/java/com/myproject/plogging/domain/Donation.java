6718bdda87c648744a1de6eec0e131f40500ca17
JIYEON <publicjy324@gmail.com>
2024-05-17 18:16:32 +0900

    class 변수 추가

--- src/main/java/com/myproject/plogging/domain/Donation.java
+++ src/main/java/com/myproject/plogging/domain/Donation.java
@@ -1,4 +1,24 @@
 package com.myproject.plogging.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    private int contribution;
 }
